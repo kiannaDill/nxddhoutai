39771e814c1158315ad16f53e6025e1af9ac6df0
悠然\wf199 <zswddx0217@163.com>
2022-08-08 16:14:24 +0800

    飞-侧边栏提交

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    // redirect: '/login'
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
