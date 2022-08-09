a961c7932edcf8013deee3bd3f0bf08f1b6ebe6f
悠然\wf199 <zswddx0217@163.com>
2022-08-09 14:59:41 +0800

    王飞8月9日提交

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
 Vue.use(ElementUI);
+import 'echarts';
+import ECharts from 'vue-echarts'
+Vue.component('ECharts', ECharts)
 
 Vue.config.productionTip = false
+
+// 引入 mock 拦截
+if (process.env.NODE_ENV === 'development') {
+  require('./mock/mock')
+}
 
 new Vue({
   router,
